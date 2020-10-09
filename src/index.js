7fdc71ff7f73fa7e899d0a490f28e6df516acc11
deraless <241473@student.pwr.edu.pl>
2020-10-09 13:42:15 +0200

    LoginApp

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const redirectUri = "https://deraless.github.io/Bezpieczne-uslugi-internetowe/"
 
 ReactDOM.render(
   <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={window.location.origin}>
+    domain={domain}
+    clientId={clientId}
+    redirectUri={redirectUri}>
     <App />
   </Auth0Provider>,
   document.getElementById('root')
