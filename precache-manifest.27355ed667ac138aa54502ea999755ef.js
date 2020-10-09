147844e2ed28f1d9cfc919b66c4563a8892d148d
deraless <241473@student.pwr.edu.pl>
2020-10-09 13:22:48 +0200

    Updates

---
+++ precache-manifest.27355ed667ac138aa54502ea999755ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71376a50568421487234625c4028230",
+    "url": "/Bezpieczne-uslugi-internetowe/index.html"
+  },
+  {
+    "revision": "c43a29cc6b8784d6b663",
+    "url": "/Bezpieczne-uslugi-internetowe/static/css/main.9af926ce.chunk.css"
+  },
+  {
+    "revision": "5f8fb6a88510522f92aa",
+    "url": "/Bezpieczne-uslugi-internetowe/static/js/2.750ea30a.chunk.js"
+  },
+  {
+    "revision": "0adf7f1b9434a120a5bce5b5824fa305",
+    "url": "/Bezpieczne-uslugi-internetowe/static/js/2.750ea30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43a29cc6b8784d6b663",
+    "url": "/Bezpieczne-uslugi-internetowe/static/js/main.bc168742.chunk.js"
+  },
+  {
+    "revision": "104fa28d7dab9fd62eb0",
+    "url": "/Bezpieczne-uslugi-internetowe/static/js/runtime-main.03a5cda0.js"
+  }
+]);
