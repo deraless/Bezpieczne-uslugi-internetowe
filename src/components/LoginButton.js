df6dec7141a0567d844b9b594cdc0f6596640438
deraless <241473@student.pwr.edu.pl>
2020-10-19 22:19:02 +0200

    LoginApp

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
     return (
-        <button onClick={() => loginWithRedirect({ redirectUri: "https://deraless.github.io/Bezpieczne-uslugi-internetowe/" })}>
-            Log in
-        </button>
+        !isAuthenticated && (
+            <button onClick={() => loginWithRedirect()}>
+                Log in
+            </button>)
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const redirectUri = "https://deraless.github.io/Bezpieczne-uslugi-internetowe/"
 
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUri={redirectUri}>
+    redirectUri={window.location.origin}>
     <App />
   </Auth0Provider>,
   document.getElementById('root')
